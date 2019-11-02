5d537c777961bd09d56e916e4a60a67bbd8bae98
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-02 21:18:35 +0200

    init

--- HowToEmbedStoryBoardsInSwiftUI/ContentView.swift
+++ HowToEmbedStoryBoardsInSwiftUI/ContentView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        CustomController()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomController: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CustomController>) -> UIViewController {
+            
+        let storyboard = UIStoryboard(name: "Custom", bundle: Bundle.main)
+        let controller = storyboard.instantiateViewController(identifier: "Home")
+        return controller
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<CustomController>) {
+        
     }
 }
